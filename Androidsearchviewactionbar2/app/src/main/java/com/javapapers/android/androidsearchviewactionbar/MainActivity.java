1b7f6b6736fef566748b340a7c06c926f8fb1f2f
immayank26 <immayanksinghal@gmail.com>
2016-02-27 22:56:14 +0530

    search
    search activity

---
+++ Androidsearchviewactionbar2/app/src/main/java/com/javapapers/android/androidsearchviewactionbar/MainActivity.java
@@ -0,0 +1,38 @@
+package com.javapapers.android.androidsearchviewactionbar;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Toast t=*/  Toast.makeText(this,"searching",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Inflate menu to add items to action bar if it is present.
+        inflater.inflate(R.menu.menu_main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
+        return true;
+    }
+ // Toast t=  Toast.makeText(this,"searching",Toast.LENGTH_SHORT).show();
+}
